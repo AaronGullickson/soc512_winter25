0cc23c5f8be8bb2090edb4668452cec852e0b895
Aaron Gullickson <aarongullickson@gmail.com>
2025-01-30 12:35:58 -0800

    Add some ggplot code with color

--- week4_making_pictures.R
+++ week4_making_pictures.R
@@ -7,7 +7,14 @@
 
 # load packages
 library(tidyverse)
-library(ggplot2)
+#library(ggplot2) - don't need this because its party of the tidyverse
 
 # load data
 load(url("https://github.com/AaronGullickson/practical_analysis/raw/master/data/nyc.RData"))
+
+
+# Make a picture ----------------------------------------------------------
+
+ggplot(nyc, aes(x = poverty, y = amtcapita))+
+  geom_point(color = "skyblue")+
+  scale_y_log10()
