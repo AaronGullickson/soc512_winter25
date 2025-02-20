90eef97d4135776792675381f0ddf84ab3990b7a
Aaron Gullickson <aarongullickson@gmail.com>
2025-02-20 10:21:22 -0800

    Add remainder of week 6 script

--- week6_reading_data.R
+++ week6_reading_data.R
@@ -8,7 +8,7 @@
 # we want the package `readr`
 library(tidyverse)
 library(haven)
-
+library(readxl)
 
 # Use relative paths! ------------------------------------------------------
 
@@ -35,4 +35,44 @@ sports <- read_csv("makebelieve_data/sports.csv",  # relative path
 
 # Reading in fixed-width text files ---------------------------------------
 
-sports <- read_fwf("makebelieve_data/sports.txt")
+# width, and starting, ending position for each variable
+# sport = 21 (1, 21)
+# team = 3 (22, 24)
+# fun_watch = 7 (25, 31)
+# number_players = 2 (32, 33)
+
+sports <- read_fwf("makebelieve_data/sports.txt",
+                   na = "..",
+                   col_positions = fwf_widths(widths = c(21, 3, 7, 2),
+                                              col_names = c("sports",
+                                                            "team",
+                                                            "fun_watch",
+                                                            "number_players")))
+
+sports <- read_fwf("makebelieve_data/sports.txt",
+                   na = "..",
+                   col_positions = fwf_cols(
+                     sports = c(1, 21),
+#                     team = c(22, 24),
+                     fun_watch = c(25, 31),
+                     number_players = c(32, 33)
+                   ))
+
+# Read in binary data -----------------------------------------------------
+
+# write our data as dta
+write_dta(sports, path = "makebelieve_data/sports.dta")
+
+# read in a dta
+sports <- read_dta("makebelieve_data/sports.dta")
+
+# read in an excel file
+sports <- read_excel("makebelieve_data/sports.xlsx",
+                     skip = 1, na = "..", sheet = 1)
+
+# Saving and loading RData ------------------------------------------------
+
+save(sports, file = "makebelieve_data/sports.RData")
+
+load("makebelieve_data/sports.RData")
+
