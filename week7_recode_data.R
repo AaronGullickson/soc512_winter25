a6122a8377b49524e15a3ecdaedc8d9dc4bddeda
Aaron Gullickson <aarongullickson@gmail.com>
2025-02-20 13:50:10 -0800

    Update week 7

--- week7_recode_data.R
+++ week7_recode_data.R
@@ -15,6 +15,7 @@ library(tidyverse)
 acs <- read_fwf("class_data/ipums_data/usa_00131.dat.gz",
                 col_positions = fwf_cols(SEX     = c(53, 53),
                                          AGE     = c(54, 56),
+                                         PERWT   = c(43, 52),
                                          MARST   = c(57, 57),
                                          RACE    = c(58, 58),
                                          HISPAN  = c(62, 62),
@@ -50,7 +51,6 @@ acs <- acs |>
 table(acs$SEX, acs$sex, exclude = NULL)
 
 # more complicated variable using case_when
-
 acs <- acs |>
   mutate(mar_stat = case_when(MARST == 1 | MARST == 2 ~ "Married",
                               MARST == 3 | MARST == 4 ~ "Separated/Divorced",
@@ -64,12 +64,32 @@ acs <- acs |>
 # check ourselves
 table(acs$MARST, acs$mar_stat, exclude = NULL)
 
+# education, detailed example
+acs <- acs |>
+  mutate(high_degree = case_when(EDUCD <= 1 | EDUCD == 999 ~ NA,
+                                 EDUCD < 62 ~ "No Degree",
+                                 EDUCD < 81 ~ "HS Diploma",
+                                 EDUCD < 101 ~ "AA Degree",
+                                 EDUCD < 114 ~ "BA Degree",
+                                 TRUE ~ "Grad Degree"),
+         high_degree = factor(high_degree,
+                              levels = c("No Degree", "HS Diploma", "AA Degree",
+                                         "BA Degree", "Grad Degree")))
 
+# check ourselves before we wreck ourselves
+table(acs$EDUCD, acs$high_degree, exclude = NULL)
 
+# age
+acs <- acs |>
+  mutate(age = ifelse(AGE == 999, NA, AGE))
+
+table(acs$AGE, is.na(acs$age), exclude = NULL)
 
 # Create final analytical data --------------------------------------------
 
 analytical_data <- acs |>
-  select(sei, sex)
+  select(sei, sex, mar_stat, high_degree, age)
+
+summary(analytical_data)
 
 save(analytical_data, file = "analytical_data.RData")
