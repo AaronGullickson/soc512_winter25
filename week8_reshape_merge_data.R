f842712dfaa824de8a8b2da5d9f5aa21072ad6c9
Aaron Gullickson <aarongullickson@gmail.com>
2025-02-27 12:33:45 -0800

    Fix world bank

--- week8_reshape_merge_data.R
+++ week8_reshape_merge_data.R
@@ -1,5 +1,5 @@
 ############################
-# Week 7                   #
+# Week 8                   #
 # Reshape/Merge Data       #
 # Soc 412/512 Winter 2025  #
 # Aaron Gullickson         #
@@ -7,7 +7,6 @@
 
 library(tidyverse)
 library(ggvenn)
-
 
 # Read in data ------------------------------------------------------------
 
@@ -26,4 +25,17 @@ vdem <- read_csv("class_data/vdem/V-Dem-CY-Full+Others-v13.csv.gz") |>
   select(country_name, country_text_id, year, v2x_libdem) |>
   filter(year == 2018 | year ==2019)
 
+
+# Fix World Bank data -----------------------------------------------------
+
+world_bank <- world_bank |>
+  select(!series_name) |>
+  mutate(series_code = case_when(
+    series_code == "NY.GDP.MKTP.CD" ~ "gdp",
+    series_code == "SP.DYN.LE00.IN" ~ "life_exp",
+    series_code == "EN.ATM.CO2E.PC" ~ "co2_percap"
+  )) |>
+  pivot_longer(cols = starts_with("year"), names_prefix = "year",
+               names_to = "year") |>
+  pivot_wider(names_from = series_code, values_from = value)
 
